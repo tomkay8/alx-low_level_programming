50c59980b2f760f2c09605ef9868e9ac8ed86e62
Kayode Thomas <thomaskayode8@gmail.com>
2023-05-03 12:05:41 -0700

    You mustn't be afraid to dream a little bigger, darling

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -9,14 +9,17 @@
 int factorial(int n)
 {
 	/* If n is negative, return -1 to indicate an error */
-	if (n < 0) {
-	return -1;
+	if (n < 0)
+	{
+	return (-1);
 	}
-	else if (n == 0) {
-	return 1;
+	else if (n == 0)
+	{
+	return (1);
 	}
-	/* Otherwise, recursively call the function with n-1 and multiply the result by n */
-	else {
-	return n * factorial(n-1);
+	/* Otherwise, call the function with n-1 and multiply the result by n */
+	else
+	{
+	return (n * factorial(n - 1));
 	}
 }
