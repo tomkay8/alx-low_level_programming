d334c62c2c937afb625e9a41950ebdbf949eb2fc
Kayode Thomas <thomaskayode8@gmail.com>
2023-05-03 13:10:31 -0700

    Your subconscious is looking for the dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,10 +1,35 @@
 #include "main.h"
+
 /**
- * _sqrt_recursion - Calculates the natural square root of a number.
- * @n: The number to calculate the square root of.
+ * actual_sqrt_recursion - Recurses to find the natural square root of a number
  *
- * Return: The natural square root of n, or -1 if n does not have a natural
- *         square root.
+ * @n: The number to calculate the square root of
+ * @y: The iterator
+ *
+ * Return: The resulting square root, or -1 if n does not have a natural square
+ *         root
+ */
+int actual_sqrt_recursion(int n, int y)
+{
+	/* If y^2 is greater than n, there is no natural square root */
+	if (y * y > n)
+	return (-1);
+
+	/* If y^2 is equal to n, we've found the natural square root */
+	if (y * y == n)
+	return (y);
+
+	/* Recurse with y + 1 to try the next possible value */
+	return (actual_sqrt_recursion(n, y + 1));
+}
+
+/**
+ * _sqrt_recursion - Returns the natural square root of a number
+ *
+ * @n: The number to calculate the square root of
+ *
+ * Return: The resulting square root, or -1 if n does not have a natural square
+ *         root
  */
 int _sqrt_recursion(int n)
 {
@@ -12,27 +37,5 @@ int _sqrt_recursion(int n)
 	if (n < 0)
 	return (-1);
 
-	if (n == 0 || n == 1)
-	return (n);
-	/**
-	 * We search for the square root of n by trying all
-	 * possible values up to n/2.
-	 */
-	int i;
-
-	for (i = 1; i <= n / 2; i++)
-	{
-	/* If we find the square root, we return it */
-	if (i * i == n)
-	return (i);
-
-	/**
-	 * If i^2 is greater than n, we know that there is no natural
-	 * square root of n.
-	 */
-	if (i * i > n)
-	return (-1);
-	}
-	/* If we reach this point, there is no natural square root of n */
-	return (-1);
+	return (actual_sqrt_recursion(n, 0));
 }
