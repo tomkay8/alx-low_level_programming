cc641565911812a78edda36de844c812912f0a70
Kayode Thomas <thomaskayode8@gmail.com>
2023-08-24 18:26:37 -0700

    a function that adds a new node at the beginning of a list_t list.

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_node - a new node is added at the beginning of a linked list
+ * @head: double pointer to the list_t list
+ * @str: new string to be placed in the node
+ *
+ * Return: the address of the new element, or NULL if addition fails
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *newNode;
+	unsigned int i = 0;
+
+	while (str[i])
+	i++;
+
+	newNode = malloc(sizeof(list_t));
+	if (!newNode)
+	return (NULL);
+
+	newNode->str = strdup(str);
+	newNode->i = i;
+	newNode->next = (*head);
+	(*head) = newNode;
+
+	return (*head);
+}
