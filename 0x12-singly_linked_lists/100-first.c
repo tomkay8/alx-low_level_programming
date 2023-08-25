5fc6dae227ddde92b549f3be70c7b567596bceff
Kayode Thomas <thomaskayode8@gmail.com>
2023-08-24 19:45:55 -0700

    The Hare and the Tortoise

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -10,5 +10,5 @@ void first(void) __attribute__((constructor));
 void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
-	printf("I carried my home on my back!\n");
+	printf("I bore my house upon my back!\n");
 }
